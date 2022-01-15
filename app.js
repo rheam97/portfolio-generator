a7ef83a474f93d2a478f3d09ab7ca09ebdad9543
Rhea <rheam97@gmail.com>
2022-01-15 10:41:41 -0500

    generated index and modularized code

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
- var profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs  = require("fs");
+const generatePage = require("./src/page-template.js")
 
-// const printProfileData = (profileDataArr) => {
-//     for (let i=0; i<profileDataArr.length; i++) {
-//         console.log(profileDataArr[i])
-//     }
-   
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [user, github] =profileDataArgs
 
-// console.log('================');
 
-// profileDataArr.forEach(profileItem =>
-//     console.log(profileItem))
-// }
-// printProfileData(profileDataArgs)
+fs.writeFile('index.html', generatePage(user, github), err => {
+    if(err) throw err
+    console.log('Portfolio complete! Check out index.html to see the output')
+})
+
