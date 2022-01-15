67ab3c0894eac0836f2adfec3965e0c6cff34c15
Rhea <rheam97@gmail.com>
2022-01-15 11:54:16 -0500

    installed inquirer, generated name input and fulfilled answer promise

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-const fs  = require("fs");
-const generatePage = require("./src/page-template.js")
+const inquirer = require("inquirer")
+// const fs  = require("fs");
+// const { generate } = require("rxjs");
+// const generatePage = require("./src/page-template.js")
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [user, github] =profileDataArgs
+// const pageHTML = generatePage(user, github)
 
 
-fs.writeFile('index.html', generatePage(user, github), err => {
-    if(err) throw err
-    console.log('Portfolio complete! Check out index.html to see the output')
-})
-
+// fs.writeFile('index.html', pageHTML, err => {
+//     if(err) throw err
+//     console.log('Portfolio complete! Check out index.html to see the output')
+// })
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    }
+]).then(answers=>console.log(answers))
